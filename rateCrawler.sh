a635bc21d454357b1a1942a732f12a8b6d395e53
SAETAK <ihjang404@gmail.com>
2025-03-18 02:07:52 +0900

    Separate error code

--- rateCrawler.sh
+++ rateCrawler.sh
@@ -1,2 +1,3 @@
 cd /root/test
-/usr/bin/mvn compile exec:java -Dexec.mainClass="com.inhoan.test.RateCrawling" >> /root/test/mvn_compile.log 2>&1
+/usr/bin/mvn compile exec:java -Dexec.mainClass="com.inhoan.test.RateCrawling" >> /root/test/stdout.log
+/usr/bin/mvn compile exec:java -Dexec.mainClass="com.inhoan.test.RateCrawling" 2>> /root/test/error.log
